69588ea5e19a23839aa7e49c68555554336b90c4
RobeOrtiz <robeortizlopez@gmail.com>
2024-03-05 10:22:10 +0100

    Version 2

--- src/contadorNumeros/ContadorMultiplosNumerosAleatorios.java
+++ src/contadorNumeros/ContadorMultiplosNumerosAleatorios.java
@@ -1,6 +1,8 @@
 package contadorNumeros;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
@@ -9,9 +11,31 @@ import java.util.TreeMap;
 public class ContadorMultiplosNumerosAleatorios {
 
 	private static Random random = new Random();
-	private static int[] multiplos = {2,3,5,7,11,13,17};
 	
+	private static boolean esPrimo(int numero) {
+		int inicio = 2;
+		boolean esPrimo = true;
+		while((esPrimo) && (inicio!= numero)) {
+			if(numero % inicio == 0)
+				esPrimo = false;
+			inicio++;			
+		}
+		return esPrimo;
+	}
 	
+	private static Integer[] generarMultiplos(int hastaDonde) {
+		List<Integer> multiplos = new ArrayList<>();
+		for(int i = 2; i <= hastaDonde;i++) {
+			if(esPrimo(i)) {
+				multiplos.add(i);
+			}
+		}
+		Integer[] arrayMultiplo = new Integer[multiplos.size()];
+		multiplos.toArray(arrayMultiplo);
+		return arrayMultiplo;
+		
+		
+	}
 	
 	public static Set<Integer> generarNumerosAleatorios() {
 		Set<Integer> numerosAleatorios = new HashSet<>();
@@ -20,12 +44,12 @@ public class ContadorMultiplosNumerosAleatorios {
 		}
 		return numerosAleatorios;
 	}
-	
-	
-	public static void conteoDeMultiplos(Set<Integer> listaDeNumeros) {
-		Map<Integer,Integer> multiplosFrecuencia = new TreeMap<>();		
+		
+	public static void conteoDeMultiplosHastaDonde(Set<Integer> listaDeNumeros, int hastaDonde) {
+		Map<Integer,Integer> multiplosFrecuencia = new TreeMap<>();
+		Integer [] multiplos2 = generarMultiplos(hastaDonde);
 		listaDeNumeros.forEach(s->{
-			for(int multiplo : multiplos) {
+			for(int multiplo : multiplos2) {
 				if(s%multiplo==0) {
 					multiplosFrecuencia.compute(multiplo,(key,value)->value==null?1:value+1);
 				}
@@ -36,15 +60,19 @@ public class ContadorMultiplosNumerosAleatorios {
 	
 	
 	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 		
 		Set<Integer> numerosAleatorios = generarNumerosAleatorios();				
-		conteoDeMultiplos(numerosAleatorios);
+		conteoDeMultiplosHastaDonde(numerosAleatorios, 60);
+		
+		
+		
 		
 	}
-	
-	
-	
-	
 }
